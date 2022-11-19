a94f7919d287e7e9305649446c1364ebd4983405
rahul3145760@gmail.com <Mygithub@1234>
2022-11-19 21:29:12 +0530

    user pojo added

---
+++ src/main/java/com/cafe/inn/POJO/User.java
@@ -0,0 +1,43 @@
+package com.cafe.inn.POJO;
+
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity
+@DynamicInsert
+@DynamicUpdate
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="contactNumber")
+    private String contactNumber;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="status")
+    private String status;
+
+    @Column(name="role")
+    private String role;
+
+
+
+}
