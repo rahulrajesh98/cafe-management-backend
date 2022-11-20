cb1778bf872304d2a35ae55e5fbedecfa2196d17
rahul3145760@gmail.com <Mygithub@1234>
2022-11-20 12:11:08 +0530

    Api not hitting problem solved

--- src/main/java/com/cafe/inn/POJO/User.java
+++ src/main/java/com/cafe/inn/POJO/User.java
@@ -7,38 +7,38 @@ import org.hibernate.annotations.DynamicUpdate;
 import javax.persistence.*;
 import java.io.Serializable;
 
-@NamedQuery(name = "User.findByEmailId",query = "select u from User u where u.email=:email")
+@NamedQuery(name = "User.findByEmailId", query = "select u from User u where u.email=:email")
 
 @Data
 @Entity
 @DynamicInsert
 @DynamicUpdate
-public class User  {
+public class User implements Serializable {
 
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id")
+    @Column(name = "id")
     private Integer id;
 
-    @Column(name="name")
+    @Column(name = "name")
     private String name;
 
-    @Column(name="contactNumber")
+    @Column(name = "contactNumber")
     private String contactNumber;
 
-    @Column(name="email")
+    @Column(name = "email")
     private String email;
 
-    @Column(name="password")
+    @Column(name = "password")
     private String password;
 
-    @Column(name="status")
+    @Column(name = "status")
     private String status;
 
-    @Column(name="role")
+    @Column(name = "role")
     private String role;
-
 
 
 }
--- src/main/java/com/cafe/inn/restImpl/UserRestImpl.java
+++ src/main/java/com/cafe/inn/restImpl/UserRestImpl.java
@@ -8,9 +8,11 @@ import com.cafe.inn.utils.CafeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@RestController
 public class UserRestImpl implements UserRest {
 
     @Autowired
