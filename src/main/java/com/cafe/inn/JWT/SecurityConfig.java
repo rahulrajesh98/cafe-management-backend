6efe141f4c6aaf2dfdb942b3181e4f076accf351
rahul3145760@gmail.com <Mygithub@1234>
2022-11-21 12:06:21 +0530

    spring boot downgraded

---
+++ src/main/java/com/cafe/inn/JWT/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.cafe.inn.JWT;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired CustomerUserDetailsService customerUserDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customerUserDetailsService);
+    }
+}
